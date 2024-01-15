ba9169d374e15d0be5f52c7dfa8a2f39cc5dd2a5
JP <10114814-moralpanic@users.noreply.gitlab.com>
2024-01-15 15:34:43 +0000

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,20 +1,26 @@
+#include <iostream>
 #include <vector>
+#include <cassert>
+#include <optional>
 #include <unordered_map>
 
-using std::vector;
-using std::unordered_map;
+using std::vector, std::tuple, std::make_tuple, std::optional, std::unordered_map;
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int> &nums, int target) {
-        unordered_map<int, int> map;
-        for (int i = 0; i < nums.size(); i++) {
-            if (map.find(target - nums.at(i)) != map.end()) {
-                return {map.at(target - nums.at(i)), i};
-            } else {
-                map[nums.at(i)] = i;
-            }
+optional<tuple<size_t, size_t>> two_sum(const vector<int> &nums, const int target) {
+    unordered_map<int64_t, size_t> map = {};
+
+    for (size_t i = 0; i < nums.size(); i++) {
+        if (map.contains(target - nums[i])) {
+            return make_tuple(map[target - nums[i]], i);
         }
-        return {-1, -1};
+        map[nums[i]] = i;
     }
-};
+
+    return std::nullopt;
+}
+
+int main() {
+    const vector nums = {3, 4, 5, 1, 8, 7, 13, 20, 21, 18};
+    assert(two_sum(nums, 20).value_or(make_tuple(-1, -1)) == make_tuple(5, 6));
+    return 0;
+}
