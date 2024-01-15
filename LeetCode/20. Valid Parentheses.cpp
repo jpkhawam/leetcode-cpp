6d285a19a0dee9b7c9befe6a75b845944bf49316
JP <10114814-moralpanic@users.noreply.gitlab.com>
2024-01-15 19:24:43 +0000

    Update 20. Valid Parentheses.cpp

--- LeetCode/20. Valid Parentheses.cpp
+++ LeetCode/20. Valid Parentheses.cpp
@@ -1,29 +1,45 @@
-#include <string>
+#include <iostream>
 #include <stack>
+#include <cassert>
 
-using std::string;
-using std::stack;
+char get_matching_parenthesis(char closing_parenthesis);
 
-class Solution {
-private:
-    char getMatchingCharacter(char c) {
-        switch (c) {
-            case ')': return '(';
-            case ']': return '[';
-            case '}': return '{';
-            default: return ' ';
+bool valid_parentheses(const std::string &str) {
+    std::stack<char> st{};
+    for (auto current_char: str) {
+        switch (current_char) {
+            case '[':
+            case '(':
+            case '{':
+                st.push(current_char);
+                break;
+            default:
+                if (st.empty() || st.top() != get_matching_parenthesis(current_char)) {
+                    return false;
+                }
+                st.pop();
         }
     }
+    return st.empty();
+}
 
-public:
-    bool isValid(string s) {
-        stack<char> st;
-        for (char c: s)
-            if (!st.empty() && getMatchingCharacter(c) == st.top())
-                st.pop();
-            else
-                st.push(c);
-        return st.empty();
+char get_matching_parenthesis(const char closing_parenthesis) {
+    switch (closing_parenthesis) {
+        case ')':
+            return '(';
+        case ']':
+            return '[';
+        case '}':
+            return '{';
+        default:
+            throw std::runtime_error("invalid character");
     }
-};
+}
 
+int main() {
+    const auto valid_str = "(())(){}";
+    const auto invalid_str = "(()(])";
+    assert(valid_parentheses(valid_str) == true);
+    assert(valid_parentheses(invalid_str) == false);
+    return 0;
+}
